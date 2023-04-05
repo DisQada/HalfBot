8bf690fe00425b903015f74fd388e39d56feca26
The Alpha <contact@nabilalsaiad.com>
2023-04-05 18:38:32 +0800

    feat(discordbot.ts): improvements, refactors and new changes
    - vars are info are new non-optional properties

--- src/core/discordBot.ts
+++ src/core/discordBot.ts
@@ -1,24 +1,31 @@
+import type { ClientOptions, Interaction } from "discord.js";
+import type { BotCommandInteraction } from "../entities/command";
+import type { Info } from "./extra";
+import type { BotStyleData } from "../config/style";
+import type { BotInfoData } from "../config/info";
+
 import {
+	ApplicationCommandType,
 	Client,
 	Collection,
-	IntentsBitField,
-	Interaction,
-	Partials
+	Events,
+	GatewayIntentBits
 } from "discord.js";
 import {
 	getFilePath,
 	getFilePathsInFolder,
 	saveMainFolders
 } from "paths-manager";
-import type { BotCommandInteraction, BotCommand } from "../entities/command";
-import type { BotEvent } from "../entities/event";
-import { BotStyle, BotStyleData } from "../config/style";
+import { BotCommand, BotCommandContextMenuType } from "../entities/command";
+import { BotEvent } from "../entities/event";
 import { checkData, Modules } from "./extra";
-import { BotInfo, BotInfoData } from "../config/info";
-import interactionCreate from "../modules/events/interactionCreate";
-import ready from "../modules/events/ready";
+import { BotStyle } from "../config/style";
+import { BotInfo } from "../config/info";
+import { BotVars } from "../config/vars";
 import { config } from "dotenv";
-import { BotVars } from "../main";
+import interactionCreate from "./events/interactionCreate";
+import ready from "./events/ready";
+import Importer from "../helpers/classes/importer";
 config();
 
 export interface DiscordBotData {
@@ -27,23 +34,27 @@ export interface DiscordBotData {
 
 export class DiscordBot {
 	public client: Client;
-	public info?: BotInfo;
-	public vars?: BotVars;
+	public vars: BotVars = {};
+	public info: BotInfo = { clientId: "" };
 	public style?: BotStyle;
 
 	public commands: Collection<string, BotCommand> = new Collection();
 
-	public constructor(data: DiscordBotData) {
+	public constructor(
+		data: DiscordBotData = {
+			rootDirectory: "bot"
+		},
+		options: ClientOptions = {
+			intents: [
+				GatewayIntentBits.Guilds,
+				GatewayIntentBits.GuildMessages,
+				GatewayIntentBits.GuildMembers
+			]
+		}
+	) {
 		// TODO Check token and clientId validity
 
-		this.client = new Client({
-			intents: [
-				IntentsBitField.Flags.Guilds,
-				IntentsBitField.Flags.GuildMessages,
-				IntentsBitField.Flags.GuildMembers
-			],
-			partials: [Partials.GuildMember, Partials.Message]
-		});
+		this.client = new Client(options);
 
 		saveMainFolders([data.rootDirectory]);
 
@@ -70,8 +81,8 @@ export class DiscordBot {
 	}
 
 	private runCoreEvents() {
-		this.client.on("ready", () => ready(this));
-		this.client.on("interactionCreate", (interaction: Interaction) => {
+		this.client.on(Events.ClientReady, () => ready(this));
+		this.client.on(Events.InteractionCreate, (interaction: Interaction) => {
 			const botInteraction: BotCommandInteraction =
 				interaction as BotCommandInteraction;
 			botInteraction.bot = this;
@@ -85,36 +96,52 @@ export class DiscordBot {
 	) {
 		const filePath = getFilePath(`${fileName}.json`);
 		if (filePath) {
-			const data = await this.importFile(filePath);
-			useData(data);
+			const data = await Importer.importFile<DataType>(filePath);
+			if (data) {
+				useData(data);
+			}
 		} else {
 			throw new Error(`Could not find ${fileName}`);
 		}
 	}
 
-	private async importFile(filePath: string) {
-		const fileData = await import(filePath);
-		return fileData?.default;
-	}
-
 	private async registerModules(module: Modules, callback: Function) {
-		const files = getFilePathsInFolder(module);
-		if (!files) throw new Error(`No files found for module: ${module}`);
+		let files = getFilePathsInFolder(module);
+		if (!files) return;
 
 		for (let i = 0; i < files.length; i++) {
 			const filePath = files[i];
 
 			if (filePath) {
-				const info = await this.importFile(filePath);
-				checkData(info, module, filePath, callback);
+				const info = await Importer.importFile(filePath);
+				if (info) {
+					checkData(info as Info, module, filePath, callback);
+				}
 			}
 		}
 	}
 
 	private async registerAllModules() {
-		await this.registerModules(Modules.Commands, (command: BotCommand) =>
-			this.commands.set(command.data.name, command)
-		);
+		await this.registerModules(Modules.Commands, (command: BotCommand) => {
+			if (command.data.types.chatInput) {
+				command.data.type = ApplicationCommandType.ChatInput;
+				this.commands.set(command.data.name, command);
+			}
+
+			if (command.data.types.contextMenu) {
+				switch (command.data.types?.contextMenu as number) {
+					case BotCommandContextMenuType.User:
+						command.data.type = ApplicationCommandType.User;
+						break;
+
+					case BotCommandContextMenuType.Message:
+						command.data.type = ApplicationCommandType.Message;
+						break;
+				}
+
+				this.commands.set(command.data.name, command);
+			}
+		});
 
 		await this.registerModules(Modules.Events, (event: BotEvent<any>) =>
 			this.client.on(event.data.name, (...args: any) =>
