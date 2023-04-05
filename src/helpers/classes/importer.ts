373dd1094aec8d6c43493348c1b984695dd4b047
The Alpha <contact@nabilalsaiad.com>
2023-04-05 18:26:01 +0800

    refactor(importer.ts): moved to it's own file
    the importer functionality was moved from being a function inside DiscordBot class to being a class
    itself

---
+++ src/helpers/classes/importer.ts
@@ -0,0 +1,8 @@
+export default class Importer {
+	public static async importFile<FileDataType>(
+		filePath: string
+	): Promise<FileDataType | void> {
+		const fileData = await import(filePath);
+		return fileData?.default;
+	}
+}
