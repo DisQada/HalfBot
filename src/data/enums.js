c64647d6a9cf638ec0dff5c1e03d4507bf21263d
The Alpha <contact@nabilalsaiad.com>
2023-09-24 12:54:48 +0800

    chore(enums): remove unnecessary comments

--- src/data/enums.js
+++ src/data/enums.js
@@ -14,17 +14,3 @@ module.exports = {
     Modules,
     RecordStates
 };
-
-// export let Modules;
-// (function (Modules) {
-//     Modules["Commands"] = "commands";
-//     Modules["Events"] = "events";
-//     Modules["Modals"] = "modals";
-// })(Modules || (Modules = {}));
-
-// export let RecordStates;
-// (function (RecordStates) {
-//     RecordStates[(RecordStates["Success"] = 0)] = "Success";
-//     RecordStates[(RecordStates["Fail"] = 1)] = "Fail";
-//     RecordStates[(RecordStates["Error"] = 2)] = "Error";
-// })(RecordStates || (RecordStates = {}));
