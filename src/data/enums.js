6c99567b8cce3debeaab7957126b24951d882f94
The Alpha <contact@nabilalsaiad.com>
2023-09-24 18:22:34 +0800

    docs(enums): add docs

--- src/data/enums.js
+++ src/data/enums.js
@@ -1,9 +1,31 @@
+/**
+ * @file
+ * @ignore
+ */
+
+/**
+ * @module Enums
+ */
+
+/**
+ * The types of bot modules.
+ * @enum {string}
+ * @property {"commands"} Commands
+ * @property {"events"} Events
+ * @property {"modals"} Modals
+ */
 const Modules = Object.freeze({
     Commands: "commands",
     Events: "events",
     Modals: "modals"
 });
 
+/**
+ * The state of a bot module registration.
+ * @enum {number}
+ * @property {0} Success
+ * @property {1} Fail
+ */
 const RecordStates = Object.freeze({
     Success: 0,
     Fail: 1
