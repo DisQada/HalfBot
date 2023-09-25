eeb2e096f7c869f9e390bb053e8cfe421eb546b8
The Alpha <contact@nabilalsaiad.com>
2023-09-25 12:07:04 +0800

    test: delete no longer needed tests

--- tests/config/info.test.js
+++
@@ -1,9 +0,0 @@
-const { BotInfo } = require("../../src/config/info");
-
-test("Info class initialising", () => {
-    const info = new BotInfo({
-        clientId: "1020917116041449492"
-    });
-
-    expect(typeof info).toBe("object");
-});
