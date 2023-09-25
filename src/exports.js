3a18eadf1f7f7d55e7858aa80a799d117356276e
The Alpha <contact@nabilalsaiad.com>
2023-09-25 12:05:04 +0800

    refactor(exports): update package exports

--- src/exports.js
+++ src/exports.js
@@ -1,21 +1,25 @@
-const discordBot = require("./core/discordBot");
-const command = require("./entities/command");
-const event = require("./entities/event");
+const { DiscordBot } = require("./core/discordBot");
+const {
+    BotCommand,
+    BotCommandContextMenuType,
+    BotCommandDeployment
+} = require("./entities/command");
+const { BotEvent } = require("./entities/event");
 const { asNumber, asString } = require("./func/colour");
 const { asEmbed, applyStyle } = require("./func/style");
 const { isSafe } = require("./func/url");
-const emojiCharacters = require("./data/emojiCharacters");
-const safeDomains = require("./data/safeDomains");
+const emojiCharacters = require("./data/emojiCharacters.json");
 
 module.exports = {
-    discordBot,
-    command,
-    event,
+    DiscordBot,
+    BotCommand,
+    BotCommandContextMenuType,
+    BotCommandDeployment,
+    BotEvent,
     asNumber,
     asString,
-    isSafe,
     asEmbed,
     applyStyle,
-    emojiCharacters,
-    safeDomains
+    isSafe,
+    emojiCharacters
 };
