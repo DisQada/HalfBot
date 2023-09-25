612a74ad163a03674482919284d6d0881d117fde
The Alpha <contact@nabilalsaiad.com>
2023-09-25 11:25:01 +0800

    docs(entities): add docs

--- src/entities/command.js
+++ src/entities/command.js
@@ -1,49 +1,89 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable jsdoc/require-example */
+/**
+ * @file
+ * @ignore
+ */
+
+/**
+ * @module Command
+ */
+
+/**
+ * @enum {number}
+ * @property {0} Global Global registration.
+ * @property {1} DevGuild Testing server only registration.
+ * @property {2} SupportGuild Support / main server registration.
+ */
 const BotCommandDeployment = Object.freeze({
     Global: 0,
     DevGuild: 1,
     SupportGuild: 2
 });
 
-// eslint-disable-next-line no-unused-vars
+/**
+ * @enum {number}
+ * @property {0} User User context menu.
+ * @property {1} Message Message context menu.
+ */
 const BotCommandContextMenuType = Object.freeze({
     User: 0,
     Message: 1
 });
 
 /**
- * @typedef {Object} BotCommandData
- * @property {string} description
- * @property {BotCommandDeployment} deployment
- * @property {string} category
- * @property {Object} types
- * @property {boolean} types.chatInput
- * @property {BotCommandContextMenuType} [types.contextMenu]
- * @extends {ChatInputApplicationCommandData | UserApplicationCommandData | MessageApplicationCommandData}
+ * @typedef {object} BotCommandData
+ * @property {string} description About the command.
+ * @property {BotCommandDeployment} deployment Where to register the command.
+ * @property {string} category The category of the command.
+ * @property {object} types The type of the command.
+ * @property {boolean} types.chatInput Whether the command is of type (chat input).
+ * @property {BotCommandContextMenuType} [types.contextMenu] What type of context menu the command is.
+ * @augments ChatInputApplicationCommandData | UserApplicationCommandData | MessageApplicationCommandData
  */
 
 /**
- * @typedef {Object} BotCommandInteraction
- * @property {DiscordBot} bot
- * @extends {ChatInputCommandInteraction | UserContextMenuCommandInteraction | MessageContextMenuCommandInteraction}
+ * @typedef {object} BotCommandInteraction
+ * @property {DiscordBot} bot The entire bot instance.
+ * @augments ChatInputCommandInteraction | UserContextMenuCommandInteraction | MessageContextMenuCommandInteraction
  */
 
 /**
  * @async
+ * @function
  * @callback BotCommandFunction
  * @param {BotCommandInteraction} interaction
- * @returns {Promise<InteractionReplyOptions | string | void>}
+ * @returns {Promise<InteractionReplyOptions | string | undefined>}
  */
 
+/**
+ * @class
+ */
 class BotCommand {
+    /**
+     * @type {BotCommandData}
+     */
     data;
+
+    /**
+     * @type {BotCommandFunction}
+     */
     execute;
 
+    /**
+     * The initialization of a new bot command.
+     * @param {BotCommandData} data - The information about the command.
+     * @param {BotCommandFunction} execute - The function to execute the command.
+     */
     constructor(data, execute) {
         this.data = data;
         this.execute = execute;
     }
 
+    /**
+     * Check a command validity.
+     * @param {BotCommand} command - The command to check.
+     * @returns {boolean} True if valid, false otherwise.
+     */
     static isValid(command) {
         if (!command.data || !command.execute) {
             return false;
--- src/entities/event.js
+++ src/entities/event.js
@@ -1,26 +1,52 @@
+/* eslint-disable jsdoc/require-example */
+/**
+ * @file
+ * @ignore
+ */
+
+/**
+ * @module Event
+ */
+
 /**
  * @template {keyof ClientEvents} Key
  * @typedef {object} BotEventData
- * @property {Key} name
+ * @property {Key} name The name of the event.
  */
 
 /**
  * @template {keyof ClientEvents} Key
  * @callback BotEventFunction
- * @param {DiscordBot} bot
+ * @param {DiscordBot} bot - The entire bot instance.
  * @param {...any} args
  * @returns {any}
  */
 
+/**
+ * @class
+ * @template {keyof ClientEvents} Key
+ */
 class BotEvent {
+    /** @type {BotEventData<Key>} */
     data;
+    /** @type {BotEventFunction<Key>} */
     execute;
 
+    /**
+     * The initialization of a new bot event.
+     * @param {BotEventData<Key>} data - The information about the event.
+     * @param {BotEventFunction<Key>} execute - The function to execute the event.
+     */
     constructor(data, execute) {
         this.data = data;
         this.execute = execute;
     }
 
+    /**
+     * Check an event validity.
+     * @param {BotEvent<any>} event - The event to check.
+     * @returns {boolean} True if valid, false otherwise.
+     */
     static isValid(event) {
         if (!event.data || !event.execute) {
             return false;
