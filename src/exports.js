99deb8ff69776b36658ef668aa1bdf9b96fad0b1
The Alpha <contact@nabilalsaiad.com>
2023-12-30 22:10:03 +0800

    refactor: Delete "emojiChars.json"
    Deleted "emojiChars.json" and all that's related to it
    
    BREAKING CHANGE: the array variable "emojiChars" has been deleted and no longer exists

--- src/exports.js
+++ src/exports.js
@@ -1,13 +1,11 @@
 const { DiscordBot } = require("./class/discordBot");
 const { asNumber, asString } = require("./func/colour");
 const { asEmbed, applyStyle } = require("./func/style");
-const emojiChars = require("./data/emojiChars.json");
 
 module.exports = {
     DiscordBot,
     asNumber,
     asString,
     asEmbed,
-    applyStyle,
-    emojiChars
+    applyStyle
 };
