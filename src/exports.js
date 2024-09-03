5dc1987bf293ff60fd725b8c8340393b30511b95
Assayyaad <hi@thealpha.pro>
2024-09-03 12:22:23 +0800

    fix: إضافة الصيغة لأسطر التصدير وتصدير بقية الأنواع

--- src/exports.js
+++ src/exports.js
@@ -1,3 +1,4 @@
-export { DiscordBot } from './class/discordBot'
-export { asNumber, asString } from './func/colour'
-export { asEmbed, applyStyle } from './func/style'
+export { DiscordBot } from './class/discordBot.js'
+export { asNumber, asString } from './func/colour.js'
+export { asEmbed, applyStyle } from './func/style.js'
+export * from './options.js'
